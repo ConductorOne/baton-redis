49d66970285f4ca4476bee43d2dccf4eb1edf301
Steve Gontzes <gontzess@gmail.com>
2026-01-27 20:56:45 -0500

    Add back WithDefaultCapabilitiesConnectorBuilder option

--- cmd/baton-redis/main.go
+++ cmd/baton-redis/main.go
@@ -10,6 +10,7 @@ import (
 	connectorSchema "github.com/conductorone/baton-redis/pkg/connector"
 	"github.com/conductorone/baton-sdk/pkg/config"
 	"github.com/conductorone/baton-sdk/pkg/connectorbuilder"
+	"github.com/conductorone/baton-sdk/pkg/connectorrunner"
 	"github.com/conductorone/baton-sdk/pkg/types"
 	"github.com/grpc-ecosystem/go-grpc-middleware/logging/zap/ctxzap"
 	"go.uber.org/zap"
@@ -25,6 +26,7 @@ func main() {
 		"baton-redis",
 		getConnector,
 		cfg.Config,
+		connectorrunner.WithDefaultCapabilitiesConnectorBuilder(&connectorSchema.Connector{}),
 	)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
